236c6149cfd2343e4161777c314b37ae1e40b113
zhangmingcheng <zhangmingcheng@pansoft.com>
2020-05-28 15:15:19 +0800

    手动生成WechatPushMessage getter、setter

--- src/main/java/ai/turbochain/ipex/entity/WechatPushMessage.java
+++ src/main/java/ai/turbochain/ipex/entity/WechatPushMessage.java
@@ -1,8 +1,5 @@
 package ai.turbochain.ipex.entity;
 
-import lombok.Data;
-
-@Data
 public class WechatPushMessage {
 	private String androidView;
 	private String title;
@@ -11,4 +8,60 @@ public class WechatPushMessage {
 	private String msgTypeName;
 	private String otcOrderBean;
 	private String loanOrderBean;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAndroidView() {
+		return androidView;
+	}
+
+	public void setAndroidView(String androidView) {
+		this.androidView = androidView;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getiOSView() {
+		return iOSView;
+	}
+
+	public void setiOSView(String iOSView) {
+		this.iOSView = iOSView;
+	}
+
+	public String getOtcOrderBean() {
+		return otcOrderBean;
+	}
+
+	public void setOtcOrderBean(String otcOrderBean) {
+		this.otcOrderBean = otcOrderBean;
+	}
+
+	public String getMsgTypeName() {
+		return msgTypeName;
+	}
+
+	public void setMsgTypeName(String msgTypeName) {
+		this.msgTypeName = msgTypeName;
+	}
+
+	public String getLoanOrderBean() {
+		return loanOrderBean;
+	}
+
+	public void setLoanOrderBean(String loanOrderBean) {
+		this.loanOrderBean = loanOrderBean;
+	}
 }
